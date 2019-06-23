5f1b90652164b1c3fa841f14a0fdfe2d22e62c8b
ankitCEO2 <tyagi.ankit59@gmail.com>
2019-06-23 11:14:21 +0530

    Adding files from local machine setup

---
+++ spring-boot-jwt/src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
@@ -0,0 +1,19 @@
+package com.javainuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author analytico
+ * <p>
+ * 23 June 2019
+ * <p>
+ * JWT Token Implementation useing Spring Security Example
+ */
+@SpringBootApplication
+public class SpringBootHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+    }
+}
